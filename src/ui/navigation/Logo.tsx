eb3bcc8828bcbc137f9dfa2d4a7942042cb01acc
nomaan-07 <nomaan07.dev@gmail.com>
2025-05-21 22:57:06 +0330

    feat: create Logo component

--- src/ui/navigation/Logo.tsx
+++ src/ui/navigation/Logo.tsx
@@ -1,5 +1,16 @@
-function Logo() {
-  return <div>Logo</div>;
+import clsx from "clsx";
+
+interface LogoProps {
+  className?: string;
+}
+
+function Logo({ className = "" }: LogoProps) {
+  const styles = clsx(
+    "font-Bungee-Spice flex items-center gap-2 text-4xl",
+    className,
+  );
+
+  return <h1 className={styles}>WatchList</h1>;
 }
 
 export default Logo;
