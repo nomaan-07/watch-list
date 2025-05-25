8d3f21cb82a741c461c2b15efebd252bb87503af
nomaan-07 <nomaan07.dev@gmail.com>
2025-05-25 23:21:21 +0330

    feat: MobileNavItem component

--- src/ui/navigation/MobileNav.tsx
+++ src/ui/navigation/MobileNav.tsx
@@ -5,12 +5,7 @@ function MobileNav() {
   return (
     <div className="fixed right-2 bottom-4 left-2 z-60 flex h-16 items-center justify-between rounded-full bg-zinc-800 px-4 shadow shadow-orange-500 sm:px-8 lg:hidden">
       {items.map((item) => (
-        <MobileNavItem
-          name={item.name}
-          icon={item.icon}
-          path={item.path}
-          key={item.name}
-        />
+        <MobileNavItem {...item} key={item.name} />
       ))}
     </div>
   );
--- src/ui/navigation/MobileNavItem.tsx
+++ src/ui/navigation/MobileNavItem.tsx
@@ -1,5 +1,22 @@
-function MobileNavItem() {
-  return <div>MobileNavItem</div>;
+import type { JSX } from "react";
+import { NavLink } from "react-router";
+
+interface MobileNavItemProps {
+  name: string;
+  icon: JSX.Element;
+  path: string;
+}
+
+function MobileNavItem({ name, icon, path }: MobileNavItemProps) {
+  return (
+    <NavLink
+      to={path}
+      className="mobile-nav-link xs:text-sm flex flex-col items-center justify-center rounded-4xl text-center text-xs capitalize"
+    >
+      {icon}
+      <span>{name}</span>
+    </NavLink>
+  );
 }
 
 export default MobileNavItem;
